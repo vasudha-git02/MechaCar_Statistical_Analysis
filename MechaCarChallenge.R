b25bd508ccf85ddc71510a3c02566f15c3db5527
vasudha-git02 <104597335+vasudha-git02@users.noreply.github.com>
2022-08-16 07:08:09 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,23 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar))
 
+#Deliverable 2
+
+#load data
+Suspension <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors=F)
+
+#summary
+total_summary <- Suspension %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), StDev=sd(PSI))
+
+#lot summary
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),StDev=sd(PSI))
+
+#Deliverable 3
+
+#t-test to determine consistency in sample means
+t.test(Suspension$PSI, mu=1500)
+
+#t-tests for individual lots
+t.test(subset(Suspension, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(Suspension, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(Suspension, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
